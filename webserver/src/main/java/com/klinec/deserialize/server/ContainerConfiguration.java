9707ac86387f23aa51788e187e380446249dee92
PaperChonChon <40238280+PaperChonChon@users.noreply.github.com>
2024-12-04 02:06:10 +0000

    Update ContainerConfiguration.java

--- webserver/src/main/java/com/klinec/deserialize/server/ContainerConfiguration.java
+++ webserver/src/main/java/com/klinec/deserialize/server/ContainerConfiguration.java
@@ -7,7 +7,7 @@ import org.slf4j.Logger;
 
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+//import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
