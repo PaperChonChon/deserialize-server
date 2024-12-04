00afc9ae89d809c880cb18c08d8700a7bb8ed962
PaperChonChon <40238280+PaperChonChon@users.noreply.github.com>
2024-12-04 02:38:55 +0000

    Update ContainerConfiguration.java

--- webserver/src/main/java/com/klinec/deserialize/server/ContainerConfiguration.java
+++ webserver/src/main/java/com/klinec/deserialize/server/ContainerConfiguration.java
@@ -6,6 +6,9 @@ import org.apache.catalina.startup.Tomcat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 // Removed the import for ConfigurableServletWebServerFactory
 
 @Configuration
