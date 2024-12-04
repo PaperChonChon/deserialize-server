31da816b4e72a528bee8748f9740d99169001482
PaperChonChon <40238280+PaperChonChon@users.noreply.github.com>
2024-12-04 04:50:53 +0000

    Update Application.java

--- webserver/src/main/java/com/klinec/deserialize/server/Application.java
+++ webserver/src/main/java/com/klinec/deserialize/server/Application.java
@@ -20,8 +20,6 @@ public class Application implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
-    //@Override
     public void run(String... args) throws Exception {
         LOG.info("Started...");
     }
